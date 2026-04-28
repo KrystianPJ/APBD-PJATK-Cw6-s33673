96fe828bf1c35d81aed890fc5175227e1ec87ae8
Krystian Kierzkowski <s33673@pjwstk.edu.pl>
2026-04-28 19:15:02 +0200

    Przygotowanie Dto

--- WebApplication1/WebApplication1/DTOs/CreateAppointmentRequestDto.cs
+++ WebApplication1/WebApplication1/DTOs/CreateAppointmentRequestDto.cs
@@ -1,6 +1,17 @@
-﻿namespace WebApplication1.DTOs;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTOs;
 
 public class CreateAppointmentRequestDto
 {
-    
+    [Required]
+    public int IdPatient { get; set; }
+    [Required]
+    public int IdDoctor { get; set; }
+    [Required]
+    public DateTime AppointmentDate { get; set; }
+    [Required, MaxLength(250)]
+    public string Reason { get; set; } = string.Empty;
 }
+
---
+++ WebApplication1/WebApplication1/DTOs/ErrorResponseDto.cs
@@ -0,0 +1,7 @@
+﻿
+namespace ClinicAdoNet.API.DTOs;
+
+public class ErrorResponseDto
+{
+    public string Message { get; set; } = string.Empty;
+}
